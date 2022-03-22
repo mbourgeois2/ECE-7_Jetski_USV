ee2219e958d9045f8cf46250aa99331786bfb49f
mbourgeois2 <mbourgeois2@umassd.edu>
2022-03-22 14:22:11 -0400

    actuator follows control stick

--- test_bench_servo_act_mega/test_bench_servo_act_mega.ino
+++ test_bench_servo_act_mega/test_bench_servo_act_mega.ino
@@ -36,167 +36,6 @@ int I_O = 0;
 int killState = 1;
 boolean done;
 
-void setup()
-{
- TCCR0B = TCCR0B & B11111000 | B00000100; //chage PWM frequency on D5 and D6 to 244.14 Hz
- pinMode(PWM_Choke, OUTPUT);
- pinMode(PWM_Throttle, OUTPUT);
- pinMode(PWM_Steer1, OUTPUT);
- pinMode(PWM_Steer2, OUTPUT);
- pinMode(Feedback_Act, INPUT);
- pinMode(Feedback_S1, INPUT);
- pinMode(Feedback_S2, INPUT);
- pinMode(D_Kill, OUTPUT);
- pinMode(D_Start, OUTPUT);
-
- THROTTLE_S.attach(PWM_Throttle);  // attaches the servo on pin 9 to the servo object
- THROTTLE_S.write(0,255,true); // set the intial position of the servo, as fast as possible, wait until done
-  
- CHOKE_S.attach(PWM_Choke);  // attaches the servo on pin 9 to the servo object
- CHOKE_S.write(0,255,true); // set the intial position of the servo, as fast as possible, wait until done
- 
-// pinMode(31, OUTPUT);
-// while (true) {
-// digitalWrite(31, HIGH);
-// delay(500);
-// digitalWrite(31, LOW);
-// delay(500);
-// }
- 
-//   for (int j = 3; j++; j<14) {
-// for (int i = 1; i++; i<256) {
-//  pinMode(j, OUTPUT);
-// analogWrite(j, i);
-// delay(20);
-// }
-//   }
-
- //------------------------------------------------------
- x8r.Begin();
- Serial.begin(9600);
- //------------------------------------------------------
-}
-
-void loop()
-{
-  //------------------------------------------------------------------------------------------------
-  //Serial.begin(9600);
-  if (x8r.Read()) {
-    for (int i = 0; i < numchannels; i++) {
-      int val = map(x8r.rx_channels()[i], 185, 1811, 0, PWM_MAX);
-      channels[i] = val;
-      if (channels[i] > PWM_MAX) {
-        channels[i] = 0;
-      }
-    }
-    //------------------------------------------------------------------------------------------
-    FeedbackVal_Steer = FeedbackRead(Feedback_Act);
-    //FeedbackVal_Choke = FeedbackRead(Feedback_S1);
-    //FeedbackVal_Throttle = FeedbackRead(Feedback_S2);
-    //Serial.println(FeedbackRead(A1));
-    //Serial.println(FeedbackVal_Steer);
-    //------------------------------------------------------------------------------------------
-    aControl(FeedbackVal_Steer, PWM_Steer1, PWM_Steer2, channels[steer]);
-    CHOKE_S.write(180*map(channels[8], 0, 255, 0, 180)/180,255,false);
-    THROTTLE_S.write(180*map(channels[throttle], 0, 255, 0, 180)/180,255,false);
-    //end control code
-    //------------------------------------------------------------------------------------------
-  }
-  else {
-    // Serial.println("nothin");
-    for (int i = 0; i < numchannels; i++) {
-      channels[i] = 0;
-    }
-  }
-  //---------------------------------------------------------------------------------------------
-}
-  
-void aControl(int w, int x, int y, int z) {
- 
-// while ((z != analogRead(w)/4 + 4) || (z != analogRead(w)/4 - 4)) {
-// if (z < 125)
-// {
-// // reverse rotation
-// analogWrite(y, 0);
-// analogWrite(x, 255);
-// }
-// if (z > 130)
-// {
-// // forward rotation
-// analogWrite(y, 255);
-// analogWrite(x, 0);
-// }
-// }
-
- //z=z*4;
- if (z < 122)
- {
- // reverse rotation
- //int reversePWM = -(z - 511) / 2;
- //int reversePWM = -(z - 127);
- analogWrite(y, 0);
- analogWrite(x, 254);
- }
- else if (z > 130)
- {
- // forward rotation
- //int forwardPWM = (z - 512) / 2;
- //int forwardPWM = (z - 127);
- analogWrite(y, 254);
- analogWrite(x, 0);
- }
- 
- else {
- if (w > 128) {
- analogWrite(y, 0);
- analogWrite(x, 254);
- }
- else if (w < 124) {
- analogWrite(y, 254);
- analogWrite(x, 0);
- }
- else if((w <= 128) && (w >= 124)) {
-  analogWrite(y, 0);
-  analogWrite(x, 0);
- }
- }
- 
-
- 
-}
-
-void sControl(int w, int x, int y) {
-  analogWrite(x,y);
-  //if ((analogRead(w)/4 + 5 != x) || (analogRead(w)/4 - 5 != x)){
-  //analogWrite(x,y);
-  //}
-}
-
-
-void ski_kill(int x, int y, int z) {
-  if (y < (PWM_MAX-1)/2) {
-    digitalWrite(x, HIGH);
-  }
-  if (y > (PWM_MAX-1)/2) {
-    digitalWrite(x, LOW);
-    digitalWrite(z, LOW);
-    I_O = 0;
-    killState = 1;
-  }
-}
-
-void ski_start(int x, int y) {
-  if ((y > (PWM_MAX-1)/2) && (killState == 0)) {
-    digitalWrite(x, HIGH);
-    I_O = 1;
-  }
-  if ((y < (PWM_MAX-1)/2)) {
-    digitalWrite(x, LOW);
-    I_O = 0;
-    killState = 0;
-  }
-}
-
 int FeedbackRead(int x) {
  int t = analogRead(x);
  int u = 0;
@@ -253,4 +92,116 @@ int FeedbackSmooth(int num)
   }
   result=mean/14;
   return(result);
+}
+
+void aControl(int w, int x, int y, int z) {
+ //int w = FeedbackRead(Feedback_Act);
+ if (w > z+2) {
+    analogWrite(y, 0);
+    analogWrite(x, 254);
+ }
+ else if (w < z-2) {
+    analogWrite(y, 254);
+    analogWrite(x, 0);
+ }
+ else if((w <= z+2) && (w >= z-2)) {
+    analogWrite(y, 0);
+    analogWrite(x, 0);
+ }
+
+}
+
+void sControl(int w, int x, int y) {
+  analogWrite(x,y);
+  //if ((analogRead(w)/4 + 5 != x) || (analogRead(w)/4 - 5 != x)){
+  //analogWrite(x,y);
+  //}
+}
+
+
+void ski_kill(int x, int y, int z) {
+  if (y < (PWM_MAX-1)/2) {
+    digitalWrite(x, HIGH);
+  }
+  if (y > (PWM_MAX-1)/2) {
+    digitalWrite(x, LOW);
+    digitalWrite(z, LOW);
+    I_O = 0;
+    killState = 1;
+  }
+}
+
+void ski_start(int x, int y) {
+  if ((y > (PWM_MAX-1)/2) && (killState == 0)) {
+    digitalWrite(x, HIGH);
+    I_O = 1;
+  }
+  if ((y < (PWM_MAX-1)/2)) {
+    digitalWrite(x, LOW);
+    I_O = 0;
+    killState = 0;
+  }
+}
+
+
+
+void setup()
+{
+ TCCR0B = TCCR0B & B11111000 | B00000100; //chage PWM frequency on D5 and D6 to 244.14 Hz
+ pinMode(PWM_Choke, OUTPUT);
+ pinMode(PWM_Throttle, OUTPUT);
+ pinMode(PWM_Steer1, OUTPUT);
+ pinMode(PWM_Steer2, OUTPUT);
+ pinMode(Feedback_Act, INPUT);
+ pinMode(Feedback_S1, INPUT);
+ pinMode(Feedback_S2, INPUT);
+ pinMode(D_Kill, OUTPUT);
+ pinMode(D_Start, OUTPUT);
+
+ //reset choke and throttle
+ THROTTLE_S.attach(PWM_Throttle);  // attaches the servo on pin 9 to the servo object
+ THROTTLE_S.write(0,255,true); // set the intial position of the servo, as fast as possible, wait until done
+  
+ CHOKE_S.attach(PWM_Choke);  // attaches the servo on pin 9 to the servo object
+ CHOKE_S.write(0,255,true); // set the intial position of the servo, as fast as possible, wait until done
+
+  
+ //------------------------------------------------------
+ x8r.Begin();
+ Serial.begin(9600);
+ //------------------------------------------------------
+}
+
+void loop()
+{
+  //------------------------------------------------------------------------------------------------
+  //Serial.begin(9600);
+  if (x8r.Read()) {
+    for (int i = 0; i < numchannels; i++) {
+      int val = map(x8r.rx_channels()[i], 185, 1811, 0, PWM_MAX);
+      channels[i] = val;
+      if (channels[i] > PWM_MAX) {
+        channels[i] = 0;
+      }
+    }
+    //------------------------------------------------------------------------------------------
+    FeedbackVal_Steer = FeedbackRead(Feedback_Act);
+    //FeedbackVal_Choke = FeedbackRead(Feedback_S1);
+    //FeedbackVal_Throttle = FeedbackRead(Feedback_S2);
+    //Serial.println(FeedbackRead(A1));
+    //Serial.println(FeedbackVal_Steer);
+    //------------------------------------------------------------------------------------------
+    aControl(FeedbackVal_Steer, PWM_Steer1, PWM_Steer2, channels[steer]);
+    CHOKE_S.write(180*map(channels[8], 0, 255, 0, 180)/180,255,false);
+    THROTTLE_S.write(180*map(channels[throttle], 0, 255, 0, 180)/180,255,false);
+    //end control code
+    //------------------------------------------------------------------------------------------
+  }
+  else {
+    // Serial.println("nothin");
+    for (int i = 0; i < numchannels; i++) {
+      channels[i] = 0;
+    }
+  }
+  //---------------------------------------------------------------------------------------------
 }
